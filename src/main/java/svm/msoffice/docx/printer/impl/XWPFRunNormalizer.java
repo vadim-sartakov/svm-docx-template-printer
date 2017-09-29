4511343d06fc2602609c62d497196dd0dc0bfe2c
sartakov <svm@job.com>
2017-09-29 12:28:48 +0500

    Splitting parameter bounds and rest run content.

--- src/main/java/svm/msoffice/docx/printer/impl/XWPFRunNormalizer.java
+++ src/main/java/svm/msoffice/docx/printer/impl/XWPFRunNormalizer.java
@@ -7,6 +7,7 @@ package svm.msoffice.docx.printer.impl;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
@@ -19,29 +20,35 @@ import org.apache.poi.xwpf.usermodel.XWPFRun;
 public class XWPFRunNormalizer {
     
     private final XWPFParagraph paragraph;
-    private final List<ParamMeta> paramList;
-    private ParamMeta param;
+    private final List<ParamLocation> paramList;
+    private ParamLocation param;
     private final String startParameterBound;
     private final String endParameterBound;
+    private final String startBoundFirstSymbol;
 
-    // Maintaining buffer in case parameter start has been splitted in different runs
+    /** Maintaining buffer in case parameter start has been splitted in different runs **/
     private final LinkedList<String> runBuffer = new LinkedList<>();
     private StringBuilder bufferResultBuilder;
-    private String bufferResult;
+    private String bufferResult = "";
     private String currentText;
-    private String textToAppend;
 
     private int index;
-    private int startBoundFirstOccurrence;
-    // For nested cases
-    private int boundOccurrences;
+    private int startBoundFirstOccurrence = -1;
+    private int endBoundFirstOccurrence = -1;
+    /** For nested cases */
+    private int boundOccurrences = 0;
+    
+    private String bound;
+    private int firstOccurrence;
+    private int offset;
 
     public XWPFRunNormalizer(XWPFParagraph paragraph, String startParameterBound, String endParameterBound) {
         this.paragraph = paragraph;
         this.startParameterBound = startParameterBound;
         this.endParameterBound = endParameterBound;
+        this.startBoundFirstSymbol = startParameterBound.substring(0, 1);
         this.paramList = new LinkedList<>();
-        param = new ParamMeta();        
+        param = new ParamLocation();        
     }
 
     public void normalizeRuns() {
@@ -49,28 +56,22 @@ public class XWPFRunNormalizer {
         replaceRuns();
     }
 
-    class ParamMeta {    
+    class ParamLocation {    
         int start = -1;
         int end = -1;
         StringBuilder paramBuilder = new StringBuilder();
     }
 
     private void collectParams() {
+        
+        // Can't change list while iterating. So, accessing elements with index.
+        List<XWPFRun> allRuns = paragraph.getRuns();
+        for (index = 0; index < allRuns.size(); index++) {
 
-        index = -1;
-        startBoundFirstOccurrence = -1;
-        boundOccurrences = 0;
+            XWPFRun run = allRuns.get(index);
+            currentText = run.getText(0);
 
-        for (XWPFRun currentRun : paragraph.getRuns()) {
-
-            index++;
-            currentText = currentRun.getText(0);
-            textToAppend = currentText;
-
-            if (currentText.contains(startParameterBound.substring(0, 1))
-                    && boundOccurrences == 0)
-                startBoundFirstOccurrence = index;
-
+            detectBoundOccurences();            
             maintainBuffer();
             checkStart();
             checkEnd();
@@ -79,17 +80,31 @@ public class XWPFRunNormalizer {
         }
 
     }
+    
+    private void detectBoundOccurences() {
+          
+        if (currentText.contains(startBoundFirstSymbol) &&
+                boundOccurrences == 0)
+            startBoundFirstOccurrence = index;
+        
+        if (currentText.contains(endParameterBound) &&
+                boundOccurrences == 1)
+            endBoundFirstOccurrence = index;
+        
+    }
 
     private void maintainBuffer() {
-
+            
         runBuffer.offer(currentText);
-
         if (runBuffer.size() > startParameterBound.length())
             runBuffer.poll();
-
+        
         bufferResultBuilder = new StringBuilder();
         runBuffer.forEach(string -> bufferResultBuilder.append(string));
         bufferResult = bufferResultBuilder.toString();
+
+        if (bufferResult.equals(startParameterBound))
+            currentText = bufferResult;
 
     }
 
@@ -97,44 +112,92 @@ public class XWPFRunNormalizer {
 
         if (bufferResult.contains(startParameterBound)) {
 
-            runBuffer.clear();
-
-            if (param.start == -1)
+            if (param.start == -1) {
+                startBoundFirstOccurrence =
+                        splitBoundAndRest(startParameterBound, startBoundFirstOccurrence, 1);                
                 param.start = startBoundFirstOccurrence;
+            }
 
-            if (bufferResult.length() == startParameterBound.length())
-                textToAppend = bufferResult;
-
+            runBuffer.clear();
             boundOccurrences++;
 
         }
 
     }
+    
+    private int splitBoundAndRest(String bound, int firstOccurrence, int offset) {
+
+        if (bufferResult.equals(startParameterBound))
+            return firstOccurrence;
+        
+        this.bound = bound;
+        this.firstOccurrence = firstOccurrence;
+        this.offset = offset;
+        
+	clearFirstOccurrenceFromBound();
+	addBoundRun();
+        
+        currentText = bound;
+        
+        return this.firstOccurrence;
+
+    }
+
+    private void clearFirstOccurrenceFromBound() {
+
+        XWPFRun firstOccurrenceRun = paragraph.getRuns().get(firstOccurrence);
+        String firstOccurrenceRunText = firstOccurrenceRun.getText(0);
+
+        String runTextWithoutBound = firstOccurrenceRunText;
+        for (int i = 0; i < bound.length(); i++)
+            runTextWithoutBound = runTextWithoutBound.replace(bound.substring(i, i + 1), "");
+        
+        firstOccurrenceRun.setText(runTextWithoutBound, 0);
+
+    }
+
+    private void addBoundRun() {
+        
+        XWPFRun firstRun = paragraph.getRuns().get(firstOccurrence);
+        firstOccurrence += offset;
+        index += offset;
+        XWPFRun newRun = paragraph.insertNewRun(firstOccurrence);
+        Utils.copyRun(firstRun, newRun);
+        newRun.setText(bound);
+        
+    }
 
     private void checkEnd() {
-        if (param.start != -1 && currentText.contains(endParameterBound))
-            if (boundOccurrences == 1)
+        
+        if (param.start != -1 && currentText.contains(endParameterBound)) {
+                        
+            if (boundOccurrences == 1) {
+                splitBoundAndRest(endParameterBound, endBoundFirstOccurrence, 0);
                 param.end = index;
-            else
+            } else
                 boundOccurrences--;
+            
+        }
+        
     }
 
     private void appendAndComplete() {
         if (param.start >= 0)
-            param.paramBuilder.append(textToAppend);       
+            param.paramBuilder.append(currentText);       
 
         if (param.end >= 0) {
             paramList.add(param);
-            param = new ParamMeta();
+            param = new ParamLocation();
             startBoundFirstOccurrence = -1;
+            endBoundFirstOccurrence = -1;
             boundOccurrences = 0;
         }
     }
 
     private void replaceRuns() {
 
-        int offset = 0;
-        for (ParamMeta currentParam : paramList) {
+        offset = 0;
+        for (ParamLocation currentParam : paramList) {
 
             // Taking in count offset after deletion
             int start = currentParam.start - offset;
